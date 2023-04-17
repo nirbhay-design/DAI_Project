1d6c9d277bb57bdbad03f1dccf4f314a00d17f36
nirbhay-design <sharma.59@iitj.ac.in>
2023-04-17 07:17:26 +0000

    adding files

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python scaffold.py configs/scaffold_c100.yaml > logfiles/scaffold_c100.log
-python fedntd.py configs/fedntd_c100.yaml > logfiles/fedntd_c100.log
+python scaffold.py configs/scaffold_amp.yaml > logfiles/scaffold_amp.log
+python fedntd.py configs/fedntd_amp.yaml > logfiles/fedntd_amp.log
